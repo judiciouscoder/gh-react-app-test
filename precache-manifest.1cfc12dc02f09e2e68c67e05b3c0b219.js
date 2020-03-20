7eec2347bc3adc3a61e4a784cc497ce6a48a5eef
jose <jose.luis.munoz@upc.edu>
2020-03-20 11:05:44 +0100

    Updates

---
+++ precache-manifest.1cfc12dc02f09e2e68c67e05b3c0b219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fe52c9d36a61d1836e84f97f6223d5",
+    "url": "/gh-react-app-test/index.html"
+  },
+  {
+    "revision": "8bbf093a9a40de32b8d9",
+    "url": "/gh-react-app-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d653105a6e81f7893f7",
+    "url": "/gh-react-app-test/static/js/2.2abc0045.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-react-app-test/static/js/2.2abc0045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbf093a9a40de32b8d9",
+    "url": "/gh-react-app-test/static/js/main.f7d4ddc2.chunk.js"
+  },
+  {
+    "revision": "54d25a02aff7deddf73f",
+    "url": "/gh-react-app-test/static/js/runtime-main.639c00ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-react-app-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
